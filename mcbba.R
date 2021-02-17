3c4baacc16dfb0ebc77f19221a361805d9cae2b7
williamwiskes <zebrahairsaloon@gmail.com>
2021-02-16 21:14:24 -0800

    Initial commit

--- mcbba.R
+++ mcbba.R
@@ -1,6 +1,8 @@
+#This script is intended to help anyone who wants to load 
+#and use data from the Marin County Breeding Bird Atlas
 library(sf) #st functions
-library(dplyr) #select
 library(tmap) #mapping
+library(dplyr) #select
 #load data
 grid <- st_read("https://mcbba.github.io/webmap/mcbba.geojson")%>% st_transform(crs = 4326)
 ebird <- read.csv("https://mcbba.github.io/webmap/ebird.csv")
@@ -10,7 +12,7 @@ tm_shape(grid) +
   tm_borders("black",alpha = 1, lwd=1.5)+
   tm_text("name", col = "black", size = 1/2)
 
-#join data
+#join data and count species
 ebird_grid <- grid %>%
   left_join(ebird, by = c("name" = "name")) %>%
   group_by(name)%>%
